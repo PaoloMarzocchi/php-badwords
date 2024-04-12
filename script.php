656543c69a1230d9a79a456c70c7d3f980db544c
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-04-12 16:53:16 +0200

    add input form in html file and print the results in php file

--- script.php
+++ script.php
@@ -1,5 +1,8 @@
 <?php 
-
+$badWord = $_GET['badword'];
+$message = $_GET['message'];
+//var_dump($message);
+$censoredMsg = str_replace($badWord,'***',$message);
 
 ?>
 
@@ -8,9 +11,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>No bad words</title>
 </head>
 <body>
-
+<h4><?php echo $message.strlen($message); ?></h4>
+<h6><?php echo $censoredMsg . strlen($censoredMsg); ?></h6>
 </body>
 </html>
