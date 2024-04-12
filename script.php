266cb7747a3f23933c03e7ff9d0ad66cc3d6b70b
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-04-12 17:30:22 +0200

    add layout style to html and php files

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php 
-$badWord = $_GET['badword'];
-$message = $_GET['message'];
+$badWord = trim($_GET['badword']);
+$message = trim($_GET['message']);
 //var_dump($message);
 $censoredMsg = str_replace($badWord,'***',$message);
 
@@ -12,9 +12,35 @@ $censoredMsg = str_replace($badWord,'***',$message);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>No bad words</title>
+    <style>
+        body{
+            text-align: center;
+        }
+        h3{
+            padding:1rem;
+            border: 1px solid black;
+            background-color: rgb(53, 51, 51);
+            color: white;
+        }
+        p{
+            padding: 0.5rem 2rem;
+            border: 1px solid black;
+            width: fit-content;
+            margin: 1rem auto;
+        }
+    </style>
 </head>
 <body>
-<h4><?php echo $message.strlen($message); ?></h4>
-<h6><?php echo $censoredMsg . strlen($censoredMsg); ?></h6>
+<h3>Original message</h3>
+<p>
+    " <?php echo $message; ?> "
+</p>
+<span>The message contain <?php echo strlen($message); ?> characters</span>
+
+<h3>Censored message</h3>
+<p>
+    " <?php echo $censoredMsg; ?> "
+</p>
+<span>The message contain <?php echo strlen($censoredMsg); ?> characters</span>
 </body>
 </html>
